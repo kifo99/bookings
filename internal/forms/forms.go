572922fc201eb0640a9cee7d0c29818f44e766d5
kifo99 <ivan123rakovic@gmail.com>
2022-07-27 15:27:58 +0200

    new update

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -1,8 +1,12 @@
 package forms
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
+
+	"github.com/asaskevich/govalidator"
 )
 
 //Valid returns returns true if there are no errors, otherwise its false
@@ -24,13 +28,40 @@ func New(data url.Values) *Form {
 	}
 }
 
+//checks for requierd fields
+func (f *Form) Requierd(fields ...string) {
+	for _, field := range fields {
+		value := f.Get(field)
+		if strings.TrimSpace(value) == "" {
+			f.Errors.Add(field, "This field cannot be blank")
+		}
+	}
+}
+
 // Has checks if form field is in post and not empty
 func (f *Form) Has(field string, r *http.Request) bool {
 	x := r.Form.Get(field)
-
 	if x == "" {
-		f.Errors.Add(field, "This field cannot be blank")
 		return false
 	}
 	return true
+}
+
+//Checks for string min length
+func (f *Form) MinLength(field string, length int, r *http.Request) bool {
+	x := r.Form.Get(field)
+
+	if len(x) < length {
+		f.Errors.Add(field, fmt.Sprintf("This field must be at least %d long", length))
+		return false
+	}
+
+	return true
+}
+
+// checks for valide email adres
+func (f *Form) IsEmail(field string) {
+	if !govalidator.IsEmail(f.Get(field)) {
+		f.Errors.Add(field, "Invalid email address")
+	}
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -56,8 +56,13 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 
 //Reservation renders make reservation page
 func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	var emptyReservation models.Reservation
+	data := make(map[string]interface{})
+	data["reservation"] = emptyReservation
+
 	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{
 		Form: forms.New(nil),
+		Data: data,
 	})
 }
 
@@ -78,7 +83,9 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 
 	form := forms.New(r.PostForm)
 
-	form.Has("first_name", r)
+	form.Requierd("first_name", "last_name", "email")
+	form.MinLength("first_name", 3, r)
+	form.IsEmail(("email"))
 
 	if !form.Valid() {
 		data := make(map[string]interface{})
@@ -139,4 +146,9 @@ func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
 //Contact renders the contact page
 func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, r, "contact.page.tmpl", &models.TemplateData{})
+}
+
+//
+func (m *Repository) ReservationSummary(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "reservation-summary.page.tmpl", &models.TemplateData{})
 }
