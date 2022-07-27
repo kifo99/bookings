54fefc5c273818f6c2788bebd18615065880ca27
kifo99 <ivan123rakovic@gmail.com>
2022-07-27 23:28:47 +0200

    New update

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/gob"
 	"log"
 	"net/http"
 	"time"
@@ -8,6 +9,7 @@ import (
 	"github.com/alexedwards/scs/v2"
 	"github.com/kifo99/bookings/internal/config"
 	"github.com/kifo99/bookings/internal/handlers"
+	"github.com/kifo99/bookings/internal/models"
 	"github.com/kifo99/bookings/internal/render"
 )
 
@@ -19,6 +21,7 @@ var session *scs.SessionManager
 // main is the main application function
 func main() {
 
+	gob.Register(models.Reservation{})
 	//change this to true when in production
 	app.InProduction = false
 
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -97,6 +97,10 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 		})
 		return
 	}
+
+	m.App.Session.Put(r.Context(), "reservation", reservation)
+
+	http.Redirect(w, r, "/reservation-summary", http.StatusSeeOther)
 }
 
 //Renders the room page
@@ -150,5 +154,20 @@ func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
 
 //
 func (m *Repository) ReservationSummary(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, r, "reservation-summary.page.tmpl", &models.TemplateData{})
+
+	reservation, ok := m.App.Session.Get(r.Context(), "reservation").(models.Reservation)
+
+	if !ok {
+		log.Println("Wcannot get item from session")
+		m.App.Session.Put(r.Context(), "error", "Can't get reservation from session")
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	data := make(map[string]interface{})
+	data["reservation"] = reservation
+
+	render.RenderTemplate(w, r, "reservation-summary.page.tmpl", &models.TemplateData{
+		Data: data,
+	})
 }
